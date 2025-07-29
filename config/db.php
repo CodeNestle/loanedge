cf1a1bc5f70e6d54a4e377e370257355ccf87751
CodeNestle <153824093+CodeNestle@users.noreply.github.com>
2025-07-29 14:56:31 +0530

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,9 +1,9 @@
 <?php
 // DB connection settings. Namma DB name "loanedge". Adjust host/user/pass as needed.
-$DB_HOST = 'localhost';
-$DB_USER = 'root';        // change if needed
-$DB_PASS = '';            // change if needed
-$DB_NAME = 'loanedge';
+$DB_HOST = 'sql311.ezyro.com';
+$DB_USER = 'ezyro_39312727';        // change if needed
+$DB_PASS = '803cdbac3c33';            // change if needed
+$DB_NAME = 'ezyro_39312727_loanedge';
 
 $mysqli = @new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 if ($mysqli->connect_errno) {
